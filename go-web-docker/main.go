1a51e6de3ebd69fcc12123d544cfd52ee2513d0e
Diego Gatti <falassionl@gmail.com>
2020-08-13 00:23:17 -0300

    added redirect to test flutter built app

--- go-web-docker/main.go
+++ go-web-docker/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "index.html", 301)
+}
 
 func main() {
-	fmt.Println("Hello World!!")
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
 }
